05d430921ef98bd3d602306bdadea647d0d62d17
Akash <akashad@live.co.uk>
2022-03-27 19:02:40 -0400

    Using MVVM

---
+++ GOATWeatherApp/ViewControllers/MainWeatherViewController.swift
@@ -0,0 +1,24 @@
+//
+//  MainWeatherViewController.swift
+//  GOATWeatherApp
+//
+//  Created by Akash Desai on 2022-03-27.
+//
+
+import UIKit
+
+class MainWeatherViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupView()
+        
+    }
+}
+
+private extension MainWeatherViewController {
+    func setupView() {
+        view.backgroundColor = .white
+    }
+}
+
